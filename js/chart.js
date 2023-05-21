5ffbb0c971a939dd4c539d3bca2e242cdb39faad
Bastian-Fuji <yutaro2001.0917@icloud.com>
2023-05-21 22:17:39 +0900

    change

--- js/chart.js
+++ js/chart.js
@@ -1,39 +1,47 @@
-var ctx = document.getElementById("myRaderChart");
+<script>
+var ctx = document.getElementById("myRadarChart");
 var myRadarChart = new Chart(ctx, {
-	type: 'radar', 
-	data: { 
-		labels: ["英語", "数学", "国語", "理科", "社会"],
-		datasets: [{
-			label: 'Aさん',
-			data: [92, 72, 86, 74, 86],
-			backgroundColor: 'RGBA(225,95,150, 0.5)',
-			borderColor: 'RGBA(225,95,150, 1)',
-			borderWidth: 1,
-			pointBackgroundColor: 'RGB(46,106,177)'
-		}, {
-			label: 'Bさん',
-			data: [73, 95, 80, 87, 79],
-			backgroundColor: 'RGBA(115,255,25, 0.5)',
-			borderColor: 'RGBA(115,255,25, 1)',
-			borderWidth: 1,
-			pointBackgroundColor: 'RGB(46,106,177)'
-		}]
-	},
-	options: {
-		title: {
-			display: true,
-			text: '試験成績'
-		},
-		scale:{
-			ticks:{
-				suggestedMin: 0,
-				suggestedMax: 100,
-				stepSize: 10,
-				callback: function(value, index, values){
-					return  value +  '点'
-				}
-			}
-		}
-	}
+  //グラフの種類
+  type: 'radar',
+  //データの設定
+  data: {
+      //データ項目のラベル
+      labels: ["総合評価", "評価１", "評価２", "評価３", "評価４"],
+      //データセット
+      datasets: [
+          {
+              label: "〇〇",
+              //背景色
+              backgroundColor: "rgba(200,112,126,0.5)",
+              //枠線の色
+              borderColor: "rgba(200,112,126,1)",
+              //結合点の背景色
+              pointBackgroundColor: "rgba(200,112,126,1)",
+              //結合点の枠線の色
+              pointBorderColor: "#fff",
+              //結合点の背景色（ホバ時）
+              pointHoverBackgroundColor: "#fff",
+              //結合点の枠線の色（ホバー時）
+              pointHoverBorderColor: "rgba(200,112,126,1)",
+              //結合点より外でマウスホバーを認識する範囲（ピクセル単位）
+              hitRadius: 5,
+              //グラフのデータ
+              data: [4,3,4,4,4]
+          }
+      ]
+  },
+ options: {
+    // レスポンシブ指定
+    responsive: true,
+    scale: {
+      ticks: {
+        // 最小値の値を0指定
+        beginAtZero:true,
+        min: 0,
+        // 最大値を指定
+        max: 5,
+      }
+    }
+  }
 });
-
+</script>
